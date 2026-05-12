1f70b5f50f8c62b0b6873b0dcb1b80811597581f
zhaizzH <zhaizzh@qq.com>
2026-05-12 14:54:06 +0800

    更新追番页面

--- packages/vitepress-plugin-bangumi/src/components/BangumiCollection.vue
+++ packages/vitepress-plugin-bangumi/src/components/BangumiCollection.vue
@@ -42,10 +42,6 @@
               <span class="card-status-tag" :class="'tag-type-' + item.type">
                 {{ statusLabel(item.type) }}
               </span>
-              <div v-if="isCompleted(item)" class="card-complete-badge">
-                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
-                已看完
-              </div>
             </div>
 
             <div class="card-body">
